08f1140385447e929923605513349d036ff3575b
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2021-12-17 07:55:08 +0330

    Create head section for all views

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,20 @@
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="apple-touch-icon" sizes="76x76" href="../admin/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="../admin/img/favicon.png">
+
+        <title>{{ config('app.name'}} | @yield('title')</title>
+
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+        <!-- Nucleo Icons -->
+        <link href="{{ asset('admin/css/nucleo.css') }}" rel="stylesheet" />
+        <!-- Font Awesome Icons -->
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <!-- Material Icons -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+        <!-- CSS Files -->
+        <link id="pagestyle" href="{{ asset('admin/css/panel.css') }}" rel="stylesheet" />
