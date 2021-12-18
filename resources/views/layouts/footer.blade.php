434a2312f71427337b77d36409017d137bff7655
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2021-12-18 06:07:31 +0330

    modify head and footer layout for admin panel

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,20 @@
         <!--   Core JS Files   -->
-        <script src="{{ asset('admin/js/core.js') }}"></script>
-        <script src="{{ asset('admin/js/panel.js') }}"></script>
-        <script src="{{ asset('admin/js/plugins.js') }}"></script>
+        <script src="{{ asset('admin/js/core/popper.min.js') }}"></script>
+        <script src="{{ asset('admin/js/core/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('admin/js/core/bootstrap.min.js') }}"></script>
+
+        <!--   Plugins JS Files   -->   
+        <script src="{{ asset('admin/js/plugins/chartjs.min.js') }}"></script>
+        <script src="{{ asset('admin/js/plugins/perfect-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('admin/js/plugins/smooth-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('admin/js/plugins/world.js') }}"></script>
+        <script src="{{ asset('admin/js/plugins/bootstrap-notify.js') }}"></script>
+        <script src="{{ asset('admin/js/plugins/Chart.extension.js') }}"></script>
 
             @yield('scripts')
 
         <!-- Github buttons -->
         <script async defer src="https://buttons.github.io/buttons.js"></script>
         <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-        <script src="../assets/js/material-dashboard.min.js?v=3.0.0"></script>
+        <script src="../admin/js/material-dashboard.min.js?v=3.0.0"></script>
+
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -11,10 +11,11 @@
         <!--     Fonts and icons     -->
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
         <!-- Nucleo Icons -->
-        <link href="{{ asset('admin/css/nucleo.css') }}" rel="stylesheet" />
+        <link href="{{ asset('admin/css/nucleo-icons.css') }}" rel="stylesheet" />
+        <link href="{{ asset('admin/css/nucleo-svg.css') }}" rel="stylesheet" />
         <!-- Font Awesome Icons -->
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
         <!-- Material Icons -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
         <!-- CSS Files -->
-        <link id="pagestyle" href="{{ asset('admin/css/panel.css') }}" rel="stylesheet" />
+        <link id="pagestyle" href="{{ asset('admin/css/material-dashboard.min.css') }}" rel="stylesheet" />
