6c364e7239cb006d4c790da5f4d6373534e55d85
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2021-12-23 12:03:36 +0330

    Separating "sidebar" view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
  --}}
             <!-- Page Sidebar -->
             <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
-                {{ $aside }}
+                @include('layouts.sidebar')
             </aside>
 
             <!-- Page Content -->
