2b91771e886864e523787442c564a7d8361c94a7
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2021-12-29 20:57:09 +0330

    Show users in admin panel

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
@@ -14,7 +15,8 @@ class AdminUserController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::with('roles')->get();
+        return view('admin.users.index', compact(['users']));
     }
 
     /**
