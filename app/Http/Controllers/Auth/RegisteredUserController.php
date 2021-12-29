71ea938a73f573f2a38a9588e47d0150239d4f1b
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2021-12-29 20:54:14 +0330

    Define default role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -44,6 +45,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $role = Role::where('name', 'General')->first();
+        $user->roles()->attach($role->id);
 
         event(new Registered($user));
 
