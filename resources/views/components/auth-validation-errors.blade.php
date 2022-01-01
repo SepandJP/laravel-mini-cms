31ce82a69696bedb661e93efa14e5b4e6ac62492
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-01-02 00:31:41 +0330

    Edit errors box

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,12 +1,14 @@
 @props(['errors'])
 
 @if ($errors->any())
+<div class="mb-3">
     @foreach ($errors->all() as $error)
-    <div class="alert alert-danger alert-dismissible text-white" role="alert">
+    <div class="alert alert-danger alert-dismissible text-white p-2 my-1" role="alert">
         <span class="text-sm">{{ $error }}</span>
-        <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+        <button type="button" class="btn-close text-lg p-1 opacity-10" data-bs-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
     </div>
     @endforeach
+</div>
 @endif
