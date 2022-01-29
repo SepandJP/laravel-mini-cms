8d64306293f912dd1a149bfa749621256cb1c7b3
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-01-30 01:39:33 +0330

    Build master layout for  admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,22 +6,29 @@
     <body class="g-sidenav-show  bg-gray-200">
         <div class="min-h-screen bg-gray-100">
 
-            @include('layouts.navigation')
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-9">
+                    @include('layouts.navigation')
+                
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                    <!-- Page Heading -->
+                    <header class="bg-white shadow">
+                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            {{ $header }}
+                        </div>
+                    </header>
                 </div>
-            </header>
+            </div>
+
 
             <!-- Page Sidebar -->
-            {{-- <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
+            <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
                 @include('layouts.sidebar')
-            </aside> --}}
+            </aside>
 
             <!-- Page Content -->
-            <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+            <main class="main-content mt-0">
                 {{ $main }}
             </main>
 
