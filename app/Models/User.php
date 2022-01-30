fd3d0bbb265287d4e88def371678f7bc35815dda
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-01-30 17:13:24 +0330

    Define relationship between users and photos models

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
