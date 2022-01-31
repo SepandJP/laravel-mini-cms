1588230a95c7a254ac6bc740fdf488a7c7e7b6e5
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-01-31 07:28:36 +0330

    Write EditUser request rules

---
+++ app/Http/Requests/UserEditRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email',
+            'name' => 'required|string',
+            'roles' => 'required',
+            'photo_id' => 'file|mimetypes:image/*',
+            'password' => 'nullable|min:6|confirmed'
+        ];
+    }
+}
