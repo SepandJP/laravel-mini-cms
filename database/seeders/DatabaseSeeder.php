87432aa618726bf7c14d44154113e158f83fa36a
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-02-09 11:25:15 +0330

    Writing Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Photo;
+use App\Models\Post;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +19,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+        ]);
+
+        User::factory()
+            ->count(20)
+            ->create();
+
+        // Get all the roles attaching up to 3 random roles to each user
+        $roles = Role::all();
+
+        // Populate the pivot table
+        User::all()->each(function ($user) use ($roles) { 
+            $user->roles()->attach(
+                $roles->random(rand(1, 2))->pluck('id')->toArray()
+            ); 
+        });
+
+        Category::factory()
+            ->count(5)
+            ->create();
+
+        Photo::factory()
+            ->count(40)
+            ->create();
+
+        Post::factory()
+            ->count(30)
+            ->state(new Sequence(
+                ['status' => '0'],
+                ['status' => '1'],
+                ))
+            ->create();
     }
 }
