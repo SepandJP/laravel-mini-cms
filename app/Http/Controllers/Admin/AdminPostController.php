352734059f4b10d748ff7910cd71e3cf9b373d7e
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-03-03 20:19:07 +0330

    Create frontend and controller fanction for new post page

--- app/Http/Controllers/Admin/AdminPostController.php
+++ app/Http/Controllers/Admin/AdminPostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostCreateRequest;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AdminPostController extends Controller
 {
@@ -26,7 +32,8 @@ class AdminPostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::pluck('title', 'id');
+        return view('admin.posts.create', compact(['categories']));
     }
 
     /**
@@ -35,9 +42,44 @@ class AdminPostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        //
+        $post = new Post();
+
+        if ($file = $request->file('index_photo'))
+        {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = new Photo();
+            $photo->name = $file->getClientOriginalName();
+            $photo->path = $name;
+            $photo->user_id = Auth::id();
+            $photo->save();
+
+            $post->photo_id = $photo->id;
+        }
+
+        $post->title = $request->title;
+        
+        if ($request->slug)
+        {
+            $post->slug = $request->slug;
+        }
+        else
+        {
+            $post->slug = Str::slug($request->title);
+        }
+
+        $post->description = $request->description;
+        $post->meta_description = $request->meta_description;
+        $post->meta_keywords = $request->meta_keywords;
+        $post->user_id = Auth::id();
+        $post->category_id = $request->category;
+        $post->save();
+
+        Session::flash('add_post', 'New post published successfully');
+
+        return redirect()->route('posts.index');
     }
 
     /**
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,96 @@
+<x-app-layout>
+
+    @section('title')
+        {{ 'New Post' }}
+    @endsection
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Write New Post') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="main">
+            <section>
+                <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
+                        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('../admin/img/illustrations/illustration-signup.jpg'); background-size: cover;">
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-10 col-md-12 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-7">
+                       
+                                <x-auth-validation-errors>
+                                </x-auth-validation-errors>
+                                
+                                <form role="form" method="POST" action="{{ route('posts.store') }}" accept-charset="UTF-8" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <!-- Title -->
+                                    <x-field class="mb-3">
+                                        <x-label for="title" :value="__('Title')" />
+                                        <x-input id="title" type="text" name="title" :value="old('title')" required autofocus />
+                                    </x-field>
+
+                                    <!-- Slug -->
+                                    <x-field class="mb-3">
+                                        <x-label for="slug" :value="__('Slug')" />
+                                        <x-input id="slug" type="text" name="slug" :value="old('slug')" />
+                                    </x-field>
+
+                                     <!-- Description -->
+                                     <x-field class="mb-3">
+                                        <x-label for="description" :value="__('Description')" />
+                                        <textarea rows="20" class="form-control" id="description" type="textarea" name="description" required>{{old('description')}}</textarea>
+                                    </x-field>
+                                    
+                                    <!-- Categories -->
+                                    {{-- <x-field class="mb-3"> --}}
+                                        <x-label for="categories" :value="__('Category')" />
+
+                                        <select class="form-select" name="category" id="category" required>
+                                                @foreach ($categories as $key => $value)
+                                                    <option value="{{$key}}">{{$value}}</option>
+                                                    {{-- <div class="form-check form-check-inline">
+                                                        <input class="form-check-input" type="checkbox" name="roleCheckbox{{$key}}" id="roleCheckbox{{$key}}" value="{{$key}}">
+                                                        <label class="form-check-label" for="roleCheckbox{{$key}}">{{$value}}</label>
+                                                      </div> --}}
+                                                @endforeach
+                                          </select>
+                                    {{-- </x-field> --}}
+
+                                    <!-- Index Photo (Avatar) -->
+                                    <x-field class="mb-3">
+                                        <x-label for="index_photo" :value="__('Index Photo')" />
+                                        <x-input id="index_pphoto" type="file" name="index_photo" accept="image/*"/>
+                                    </x-field>
+
+                                     <!-- Meta Description -->
+                                     <x-field class="mb-3">
+                                        <x-label for="meta_description" :value="__('Meta Description')" />
+                                        <x-input id="meta_description" type="text" name="meta_description" :value="old('meta_description')" />
+                                    </x-field>
+
+                                     <!-- Meta Keywords -->
+                                     <x-field class="mb-3">
+                                        <x-label for="meta_keywords" :value="__('Meta Keywords')" />
+                                        <x-input id="meta_keywords" type="text" name="meta_keywords" :value="old('meta_keywords')" />
+                                    </x-field>
+
+                                    <div class="text-center pt-0 px-lg-2 px-1">
+                                        <div class="text-center pt-0 px-lg-2 px-1">
+                                            <x-button class="btn-lg btn-lg mt-4 mb-0">
+                                                {{ __('Publish') }}
+                                            </x-button>
+                                        </div>
+                                    </div>
+                                </form>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+    </x-slot>
+
+</x-app-layout>
