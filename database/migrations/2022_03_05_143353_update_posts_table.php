a409e62434a71a955d6ee83b7d614413abbb91d8
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-03-05 21:39:59 +0330

    Update posts table

---
+++ database/migrations/2022_03_05_143353_update_posts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn(['title', 'meta_description', 'meta_keywords']);
+            $table->dropConstrainedForeignId('user_id');
+            $table->dropConstrainedForeignId('photo_id');
+            $table->dropConstrainedForeignId('category_id');
+        });
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('photo_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['photo_id']);
+            $table->dropForeign(['category_id']);
+            $table->dropIfExists();
+        });
+    }
+}
