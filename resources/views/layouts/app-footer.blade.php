798df2c1fc2b151f44566b90a05b3bca007da668
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-03-09 19:46:50 +0330

    Add a footer to admin panel pages

--- resources/views/layouts/app-footer.blade.php
+++ resources/views/layouts/app-footer.blade.php
@@ -1,4 +1,35 @@
-        
+<footer class="footer py-4  ">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6 mb-lg-0 mb-4">
+          <div class="copyright text-center text-sm text-muted text-lg-start">
+            © <script>
+              document.write(new Date().getFullYear())
+            </script>,
+            made with <i class="fa fa-heart"></i> by
+            <a href="https://www.redl.ink/sepand" class="font-weight-bold" target="_blank">Sepand JamshidPour</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            <li class="nav-item">
+              <a href="https://www.linkedin.com/in/sepandJP" class="nav-link text-muted" target="_blank">Linkedin</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://github.com/SepandJP" class="nav-link text-muted" target="_blank">GithHub</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://codepen.io/sepand" class="nav-link text-muted" target="_blank">Codepen</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.t.me/SepandJP" class="nav-link pe-0 text-muted" target="_blank">Telegram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
         @yield('footer')
         
         <!--   Core JS Files   -->
