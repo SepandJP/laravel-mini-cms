66c79a91ad163069c6f0231091a1a470858c4d38
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-03-10 15:54:09 +0330

    Fix cards height

--- resources/views/admin/photos/index.blade.php
+++ resources/views/admin/photos/index.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Posts') }}
+            {{ __('Photos') }}
         </h2>
     </x-slot>
 
     @section('title')
-    {{ 'Posts' }}
+    {{ 'Photos' }}
     @endsection
 
     @section('styles')
@@ -20,7 +20,7 @@
                   <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                       <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                        <h6 class="text-white text-capitalize ps-3">{{--{{ Auth::user()->name }}'s--}} posts</h6>
+                        <h6 class="text-white text-capitalize ps-3">{{--{{ Auth::user()->name }}'s--}} Photos</h6>
                       </div>
                     </div>
                     <div class="mt-5 pt-5">
@@ -28,23 +28,27 @@
                           <x-auth-session-status class="alert-danger" :status="__('delete_photo')" >
                           </x-auth-session-status>
 
-                    <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3 p-4">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 p-4">
                         @foreach ($photos as $photo)
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card shadow-sm h-100">
                                     <img class="bd-placeholder-img card-img-top" alt="{{ $photo->name }}" height="200px" src="{{$photo->path}}">
                   
                                     <div class="card-body text-center">
-                                        <p>User: {{ $photo->user->name }}</p>
+                                        <div class="h-50">
+                                            <p>Uploaded by: <a href="#">{{ $photo->user->name }}</a></p>
+                                        </div>
                                         <!-- Delete Post -->
-                                        <form role="form" method="POST" action="{{ route('photos.destroy', $photo->id) }}">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-link btn-lg text-red me-auto border-0" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete">
-                                                <span class="material-icons md-48">delete</span>
-                                            </button>
-                                        </form>
+                                        <div>
+                                            <form role="form" method="POST" action="{{ route('photos.destroy', $photo->id) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-link btn-lg text-red me-auto border-0" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
+                                                    <span class="material-icons md-48">delete</span>
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
