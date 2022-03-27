69c8d26010c843b5210720f6f736b94e46ed9baf
Sepand JamshidPour <12715168+SepandJP@users.noreply.github.com>
2022-03-27 08:21:10 +0430

    Removed icon from session status component

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -2,11 +2,6 @@
 
 @if (Session::has($status))    
     <div {{ $attributes->merge(['class' => 'w-90 mx-auto alert alert-dismissible fade show']) }} role="alert">
-        <span class="alert-icon align-middle">
-        <span class="material-icons text-md">
-        thumb_up_off_alt
-        </span>
-        </span>
         <span class="alert-text">{{session($status)}}</span>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
